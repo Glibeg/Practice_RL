ea74e2b840191bf3fc1bbb11b88590c5d305c834
Glibeg <77716964+Glibeg@users.noreply.github.com>
2021-08-31 19:41:03 +0900

    Update KL_regularized.py

--- KL_regularized.py
+++ KL_regularized.py
@@ -28,7 +28,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 #torch.set_default_dtype(torch.float32)
 Transition = namedtuple('Transition',
-                        ('state', 'action', 'next_state', 'reward', 'done'))
+                        ('state', 'action', 'next_state', 'reward'))
 
 class ReplayMemory(object):
     def __init__(self, capacity):
@@ -99,14 +99,14 @@ class ValueNet(nn.Module):
 class KL():
     def __init__(self, states_dim, default_states_dim, action_space):
         self.learning_rate = 0.0005
-        self.memory_capacity = 20000
+        self.memory_capacity = int(2e+6)
         self.batch_size = 64
         self.action_space = action_space
         self.alpha = 0.1
         self.gamma = 0.9
         self.polyak = 0.995
-        self.update_every = 1
-        self.update_after = 1000
+        self.update_every = 50
+        self.update_after = 5000
         self.render = True
         self.default_states_dim = default_states_dim
 
@@ -147,7 +147,6 @@ class KL():
             transitions = self.replay_buffer.sample(self.batch_size)
             batch = Transition(*zip(*transitions))
             next_state_batch = torch.cat(batch.next_state)
-            done_batch = torch.cat(batch.done)
             state_batch = torch.cat(batch.state)
             action_batch = torch.cat(batch.action)
             reward_batch = torch.cat(batch.reward)
@@ -179,7 +178,7 @@ class KL():
                 target_action = next_pi_distribution_target.sample()
                 target_action = torch.max(torch.min(target_action, torch.tensor(self.action_space.maximum, dtype=torch.float32).to(device)), torch.tensor(self.action_space.minimum, dtype=torch.float32).to(device))
                 value_target = self.value_net_target(next_state_batch, target_action)
-                td_target = reward_batch + self.gamma * (1 - done_batch) * (value_target - self.alpha * next_kl_div_term_target.detach()) - self.alpha * kl_div_term_target.detach() 
+                td_target = reward_batch + self.gamma * (value_target - self.alpha * next_kl_div_term_target.detach()) - self.alpha * kl_div_term_target.detach() 
             
             self.policy_optim.zero_grad()
             action = pi_distribution.rsample()
@@ -218,11 +217,13 @@ def state_preprocessing(state):
     return torch.tensor(state.copy(), dtype = torch.float32).unsqueeze(0).to(device)
 
 parser = argparse.ArgumentParser()
+parser.add_argument('-domain', action = 'store', default= 'quadruped')
+parser.add_argument('-task', action = 'store', default= 'fetch')
 parser.add_argument('--test', action = 'store_true')
 args = parser.parse_args()
 
 #env = gym.make('FetchReach-v1')
-env = suite.load(domain_name = 'quadruped', task_name = 'fetch', environment_kwargs = {'flat_observation' : True})#, task_kwargs = {'time_limit' : 40})
+env = suite.load(domain_name = args.domain, task_name = args.task, environment_kwargs = {'flat_observation' : True}, task_kwargs = {'time_limit' : 40})
 #env.env.reward_type = 'dense'
 #env = gym.wrappers.filter_observation.FilterObservation(env, filter_keys=['observation', 'desired_goal'])
 #env = gym.wrappers.flatten_observation.FlattenObservation(env)
@@ -237,11 +238,11 @@ def policy_by_agent(time_step):
     return torch_action.squeeze(0).cpu().detach().numpy()
 
 if args.test:
-    agent.policy_net.load_state_dict(torch.load('./pnet.pth'))
+    agent.policy_net.load_state_dict(torch.load(f'./{args.domain}_{args.task}_pnet.pth'))
     viewer.launch(env, policy = policy_by_agent)
     exit(0)
 
-initial_count = 0
+interaction_count = 0
 num_episodes = 1000
 for e in range(num_episodes):
     done = False
@@ -252,10 +253,9 @@ for e in range(num_episodes):
     state = state_preprocessing(time_step.observation['observations'])
 
     for c in count():
-        if initial_count <= agent.update_after:
+        if interaction_count <= agent.update_after:
             action = np.random.uniform(env.action_spec().minimum, env.action_spec().maximum, env.action_spec().shape)
             action = torch.tensor(action, dtype=torch.float32).unsqueeze(0).to(device)
-            initial_count += 1
         else:
             action = agent.get_action(state)
         
@@ -265,28 +265,28 @@ for e in range(num_episodes):
         #n_o = time_step.observation['observations']
         next_state = state_preprocessing(time_step.observation['observations'])
         r = time_step.reward
-        done = 1.0 if time_step.step_type == 2 else 0.0
         score += r
-        done = torch.tensor([[done]]).to(device)
 
         #info_gain, log_likelihood = vime.calc_info_gain(o,a,n_o)
         #vime.memorize_episodic_info_gains(info_gain)
         #r = vime.calc_curiosity_reward(r, info_gain)
         reward = torch.tensor([[r]], dtype=torch.float32).to(device)
 
-        agent.replay_buffer.push(state, action, next_state, reward, done)
+        agent.replay_buffer.push(state, action, next_state, reward)
         state = next_state
         #o = n_o
         
-        if c % agent.update_every == 0:
+        if interaction_count % agent.update_every == 0 and interaction_count > agent.update_after:
             qloss, ploss = agent.train_model()
             if qloss != None:
                 qloss_list.append(qloss)
             if ploss != None:
                 ploss_list.append(ploss)
-
+        interaction_count += 1
         if time_step.step_type == 2:
-            print("episode : {:3d} | end at : {:3d} steps | score : {:3.3f} | q_loss = {:3.3f} | p_loss = {:.3f} ".format(e, c, score, np.mean(qloss_list), np.mean(ploss_list)))
+            qloss_avg = 0.0 if len(qloss_list) == 0 else np.mean(qloss_list)
+            ploss_avg = 0.0 if len(ploss_list) == 0 else np.mean(ploss_list)
+            print("episode : {:3d} | end at : {:3d} steps | score : {:3.3f} | q_loss = {:3.3f} | p_loss = {:.3f} ".format(e, c, score, qloss_avg, ploss_avg))
             break
     #for _ in range(100):
     #    transitions = agent.replay_buffer.sample(agent.batch_size)
@@ -295,6 +295,7 @@ for e in range(num_episodes):
     #    state_batch = torch.cat(batch.state)
     #    action_batch = torch.cat(batch.action)
     #    elbo = vime.update_posterior(state_batch, action_batch, next_state_batch)
-    torch.save(agent.policy_net.state_dict(), './pnet.pth')
-    torch.save(agent.value_net.state_dict(), './vnet.pth')
+    torch.save(agent.policy_net.state_dict(), f'./{args.domain}_{args.task}_pnet.pth')
+    torch.save(agent.default_policy_net.state_dict(), f'./{args.domain}_{args.task}_dpnet.pth')
+    torch.save(agent.value_net.state_dict(), f'./{args.domain}_{args.task}_vnet.pth')
 env.close()
